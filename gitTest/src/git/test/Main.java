24d492657ba6c7fb146592c250925dd342be477f
kjs09 <kjs09@LAPTOP-MNP428G2>
2019-01-10 20:10:20 +0900

    커밋1

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int age=19;
 		System.out.println("두번째 수정내용");
 		System.out.println("이거 충돌나야대??");
 		
