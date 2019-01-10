d71b820379e85625c0f2b9da78b826df4ed8b012
kjs09 <kjs09@LAPTOP-MNP428G2>
2019-01-10 19:30:51 +0900

    세번째 커밋

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("첫번째 수정내용");
-
+		System.out.println("두번째 수정내용");
+		
 	}
 
 }
